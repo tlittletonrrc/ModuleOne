710d632876b98bb0da59b3bce8393282d690acc1
Thomas Littleton <tlittleton@academic.rrc.ca>
2025-09-07 09:32:53 -0500

    added performance summary criteria to portfolio performance.

--- src/portfolio/portfolioPerformance.ts
+++ src/portfolio/portfolioPerformance.ts
@@ -8,8 +8,26 @@ function calculatePortfolioPerformance() {
 
     let performanceSummary: string;
     switch (true) {
-        case percentageChange > 20:
+        case percentageChange >= 20:
             performanceSummary = `The portfolio has gained significantly with a profit of $${profitOrLoss}.`;
+            break;
+        case percentageChange >= 10 && percentageChange <= 19.99:
+            performanceSummary = `The portfolio has gained moderately with a profit of $${profitOrLoss}.`;
+            break;
+        case percentageChange >= 0.1 && percentageChange <= 9.99:
+            performanceSummary = `The portfolio has gained slightly with a profit of $${profitOrLoss}.`;
+            break;
+        case percentageChange == 0:
+            performanceSummary = `The portfolio has no changes.`;
+            break;
+        case percentageChange <= -0.1 && percentageChange >= -9.99:
+            performanceSummary = `The portfolio has lost slightly with a loss of $${profitOrLoss}.`;
+            break;
+        case percentageChange <= -10 && percentageChange > -20:
+            performanceSummary = `The portfolio has lost moderately with a loss of $${profitOrLoss}.`;
+            break;
+        case percentageChange <= -20:
+            performanceSummary = `The portfolio has lost significantly with a loss of $${profitOrLoss}.`;
             break;
         default:
             performanceSummary = `The portfolio has performed poorly.`;
